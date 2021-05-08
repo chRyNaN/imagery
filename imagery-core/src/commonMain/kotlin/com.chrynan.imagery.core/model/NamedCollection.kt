87ed3dde35bc7a9587f08224091655167c57ca87
Chris <ckeenan@starry.com>
2021-05-08 17:20:26 -0500

    Added KDoc comments to the NamedCollection class

--- imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/model/NamedCollection.kt
+++ imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/model/NamedCollection.kt
@@ -4,7 +4,19 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
+import com.chrynan.imagery.core.util.namedListOf
+import com.chrynan.imagery.core.util.namedSetOf
 
+/**
+ * A [Collection] that contains an optional [name] property. This class is serializable. To obtain
+ * an instance of this class, either use the constructor directly or use one of the specific
+ * Collection constructor utility functions, such as [namedListOf] and [namedSetOf].
+ *
+ * Note that this class implements the [Collection] interface and delegates to the [values]
+ * property. However, this class has two generic type parameters: T (the type of each value in the
+ * [values] Collection) and C (the type of the Collection used [List, Set, etc]). So for specific
+ * Collection type functionality, use the [values] property directly.
+ */
 @ExperimentalSerializationApi
 @Serializable
 data class NamedCollection<T, C : Collection<T>>(
