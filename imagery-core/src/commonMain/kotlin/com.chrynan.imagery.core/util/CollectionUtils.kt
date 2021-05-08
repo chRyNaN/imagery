eefa9992eee18ed576a399dbad67117426737db5
Chris <ckeenan@starry.com>
2021-05-08 17:10:42 -0500

    Updated CollectionUtils

--- imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/util/CollectionUtils.kt
+++ imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/util/CollectionUtils.kt
@@ -3,25 +3,30 @@
 package com.chrynan.imagery.core.util
 
 import com.chrynan.imagery.core.model.NamedCollection
+import kotlinx.serialization.ExperimentalSerializationApi
 
+@ExperimentalSerializationApi
 fun <T> namedListOf(name: String? = null, values: List<T>): NamedCollection<T, List<T>> =
     NamedCollection(
         name = name,
         values = values
     )
 
+@ExperimentalSerializationApi
 fun <T> namedListOf(name: String? = null, vararg values: T): NamedCollection<T, List<T>> =
     NamedCollection(
         name = name,
         values = values.toList()
     )
 
+@ExperimentalSerializationApi
 fun <T> namedSetOf(name: String? = null, values: Set<T>): NamedCollection<T, Set<T>> =
     NamedCollection(
         name = name,
         values = values
     )
 
+@ExperimentalSerializationApi
 fun <T> namedSetOf(name: String? = null, vararg values: T): NamedCollection<T, Set<T>> =
     NamedCollection(
         name = name,
