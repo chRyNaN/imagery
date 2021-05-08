ffde2c5aad62750899ba913b52c1c6b656884579
Chris <ckeenan@starry.com>
2021-05-08 17:09:10 -0500

    Updated Kotlinx Serialization library and added JsonNames annotation to values property in NamedCollection

--- imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/model/NamedCollection.kt
+++ imagery-core/src/commonMain/kotlin/com.chrynan.imagery.core/model/NamedCollection.kt
@@ -1,10 +1,13 @@
 package com.chrynan.imagery.core.model
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
+@ExperimentalSerializationApi
 @Serializable
 data class NamedCollection<T, C : Collection<T>>(
     @SerialName(value = "name") val name: String? = null,
-    @SerialName(value = "values") val values: C
+    @SerialName(value = "values") @JsonNames(names = ["elements", "variants"]) val values: C
 ) : Collection<T> by values
